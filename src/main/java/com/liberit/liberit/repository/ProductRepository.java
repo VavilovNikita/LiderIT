914266aa5025ebe00075c2f0166bc7e5194e311a
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-16 03:09:58 +0500

    add DB commit

---
+++ src/main/java/com/liberit/liberit/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.liberit.liberit.repository;
+
+import com.liberit.liberit.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
