c5a2e180aaff262efa887440a4ccf0eb4cb1aeb6
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-15 20:20:01 +0500

    Initial commit

---
+++ src/main/java/com/liberit/liberit/LiberItApplication.java
@@ -0,0 +1,13 @@
+package com.liberit.liberit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiberItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiberItApplication.class, args);
+    }
+
+}
