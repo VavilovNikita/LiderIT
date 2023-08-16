358366fdb06d85c9eba6443004716132a7f2e2e1
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-17 02:05:22 +0500

    fix name commit

--- src/main/java/com/liderit/liderit/LiderItApplication.java
+++ src/main/java/com/liderit/liderit/LiderItApplication.java
@@ -1,13 +1,13 @@
-package com.liberit.liberit;
+package com.liderit.liderit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiberItApplication {
+public class LiderItApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LiberItApplication.class, args);
+        SpringApplication.run(LiderItApplication.class, args);
     }
 
 }
--- src/test/java/com/liderit/liderit/LiberItApplicationTests.java
+++ src/test/java/com/liderit/liderit/LiberItApplicationTests.java
@@ -1,4 +1,4 @@
-package com.liberit.liberit;
+package com.liderit.liderit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
