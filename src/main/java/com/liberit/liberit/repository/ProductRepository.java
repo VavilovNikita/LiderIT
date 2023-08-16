79d62cdc6c6f2232f53a05f9027822353418484e
VavilovNikita <112397078+VavilovNikita@users.noreply.github.com>
2023-08-17 02:08:21 +0500

    fix name commit

--- src/main/java/com/liberit/liberit/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.liberit.liberit.repository;
-
-import com.liberit.liberit.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
--- src/main/java/com/liberit/liberit/repository/ShowcaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.liberit.liberit.repository;
-
-import com.liberit.liberit.entity.Showcase;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShowcaseRepository extends JpaRepository<Showcase,Integer> {
-}
